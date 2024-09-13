898a540154d4707682660ad493f908712b1b89a0
Akula Kalyanram <kalyanramakula13@gmail.com>
2024-09-13 23:38:03 +0530

    Solved: 0145. Binary Tree Postorder Traversal

---
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        helper(root,result);
+        return result;
+    }
+    public void helper(TreeNode node,List<Integer> list){
+        if(node==null){
+            return;
+        }
+        
+        helper(node.left,list);
+        helper(node.right,list);
+        list.add(node.val);
+    }
+}
