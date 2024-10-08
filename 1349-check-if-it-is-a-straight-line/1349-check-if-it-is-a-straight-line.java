076168c685bd241232778234d4444b035ef4b07f
Akula Kalyanram <kalyanramakula13@gmail.com>
2024-10-08 12:56:16 +0530

    Solved: 1349. Check If It Is a Straight Line

---
+++ 1349-check-if-it-is-a-straight-line/1349-check-if-it-is-a-straight-line.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean checkStraightLine(int[][] coordinates) {
+        if(coordinates.length<2){
+            return false;
+        }
+
+        int x0=coordinates[0][0];
+        int y0=coordinates[0][1];
+        int x1=coordinates[1][0];
+        int y1=coordinates[1][1];
+        int idx=x1-x0;
+        int idy=y1-y0;
+        for(int i=2;i<coordinates.length;i++){
+            int x=coordinates[i][0];
+            int y=coordinates[i][1];
+            if((y-y0)*idx!=(x-x0)*idy){
+                return false;
+            }
+        }
+        return true;
+    }
+}
