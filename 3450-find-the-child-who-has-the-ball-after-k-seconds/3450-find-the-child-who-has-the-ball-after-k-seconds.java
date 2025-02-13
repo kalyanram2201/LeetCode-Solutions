e6a5584d886ee14c92185ebf35e983a295e5ea1c
Akula Kalyanram <kalyanramakula13@gmail.com>
2025-02-13 17:54:36 +0530

    Solved: 3450. Find the Child Who Has the Ball After K Seconds

---
+++ 3450-find-the-child-who-has-the-ball-after-k-seconds/3450-find-the-child-who-has-the-ball-after-k-seconds.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int numberOfChild(int n, int k) {
+         int pos = 0; 
+        int dir=1; 
+
+        for (int i=0;i<k;i++) {
+            pos+= dir;
+
+           
+            if (pos== n-1 || pos==0) {
+                dir *=-1;
+            }
+        }
+        
+        return pos;
+    }
+        
+}
