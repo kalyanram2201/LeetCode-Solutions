4c7412ed8dd301a57160b50a13c2a08fcd9fcbe6
Akula Kalyanram <kalyanramakula13@gmail.com>
2025-02-14 22:29:41 +0530

    Solved: 0347. Top K Frequent Elements

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int num:nums){
+            map.put(num,map.getOrDefault(num,0)+1);
+        }
+        List<Integer> sortedList = new ArrayList<>(map.keySet());
+        sortedList.sort((a, b) -> map.get(b) - map.get(a)); 
+        int result[]=new int[k];
+        for(int i=0;i<k;i++){
+            result[i]=sortedList.get(i);
+        }
+        return result;
+    }
+}
