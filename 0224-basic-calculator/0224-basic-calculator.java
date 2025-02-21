d42a5f4fafa336454be68b4cbef134d3950c1f25
Akula Kalyanram <kalyanramakula13@gmail.com>
2025-02-21 08:30:54 +0530

    Solved: 0224. Basic Calculator

---
+++ 0224-basic-calculator/0224-basic-calculator.java
@@ -0,0 +1,44 @@
+import java.util.Stack;
+
+class Solution {
+    public int calculate(String s) {
+        Stack<Integer> stack = new Stack<>();
+        int num = 0;
+        int sign = 1; 
+        int result = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (Character.isDigit(c)) {
+               
+                num = num * 10 + (c - '0');
+            } else if (c == '+') {
+               
+                result += sign * num;
+                num = 0;
+                sign = 1; 
+            } else if (c == '-') {
+                result += sign * num;
+                num = 0;
+                sign = -1; 
+            } else if (c == '(') {
+                
+                stack.push(result);
+                stack.push(sign);
+                
+                result = 0;
+                sign = 1;
+            } else if (c == ')') {
+                result += sign * num; 
+                num = 0;
+                
+                result *= stack.pop(); 
+                result += stack.pop(); 
+            }
+        }
+       
+        result += sign * num;
+        return result;
+    }
+}
