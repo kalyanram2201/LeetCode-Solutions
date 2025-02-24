20daf737f6e8bf97be956bc33b11ce9d4938a6ec
Akula Kalyanram <kalyanramakula13@gmail.com>
2025-02-24 08:25:13 +0530

    Solved: 3723. Sum of Good Numbers

---
+++ 3723-sum-of-good-numbers/3723-sum-of-good-numbers.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int sumOfGoodNumbers(int[] nums, int k) {
+        if(nums.length==0){
+            return 0;
+        }
+        int n=nums.length;
+        int sum=0;
+        for(int i=0;i<n;i++){
+             boolean isGood = true;
+
+            if (i-k>=0 && nums[i]<=nums[i-k]){
+                isGood=false;
+            }
+
+            if (i+k<n&&nums[i]<=nums[i+k]) {
+                isGood=false;
+            }
+            if(isGood){
+                sum+=nums[i];
+            }
+        }
+        return sum;
+    }
+}
