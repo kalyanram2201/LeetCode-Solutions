dce1ab3a39c05ed89111d937218937c4125b5771
Akula Kalyanram <kalyanramakula13@gmail.com>
2025-02-26 08:32:35 +0530

    Solved: 2159. Two Out of Three

---
+++ 2159-two-out-of-three/2159-two-out-of-three.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<Integer> twoOutOfThree(int[] nums1, int[] nums2, int[] nums3) {
+        int[] arr1=new int[101];
+        int[] arr2=new int[101];
+        int[] arr3=new int[101];
+        for(int num:nums1)arr1[num]=1;
+        for(int num:nums2)arr2[num]=1;
+        for(int num:nums3)arr3[num]=1;
+        List<Integer>list=new ArrayList<>();
+        for(int i=0;i<101;i++){
+            if(arr1[i]+arr2[i]+arr3[i]>1){
+                list.add(i);
+            }
+        }
+        return list;
+    }
+}
