67defb2fa1214c9775c7b5eb53adbbfada5a8dde
Akula Kalyanram <kalyanramakula13@gmail.com>
2025-03-23 17:34:55 +0530

    Solved: 0493. Reverse Pairs

---
+++ 0493-reverse-pairs/0493-reverse-pairs.java
@@ -0,0 +1,43 @@
+class Solution {
+    public  int reversePairs(int[] nums) {
+        return mergeSort(nums, 0, nums.length - 1);
+    }
+
+    private int mergeSort(int[] nums, int left, int right) {
+        if (left >= right) return 0;
+        int mid = (left + right) / 2;
+        int count = mergeSort(nums, left, mid) + mergeSort(nums, mid + 1, right);
+        count += countReversePairs(nums, left, mid, right);
+        merge(nums, left, mid, right);
+        return count;
+    }
+
+    private int countReversePairs(int[] nums, int left, int mid, int right) {
+        int count = 0, j = mid + 1;
+        for (int i = left; i <= mid; i++) {
+            while (j <= right && nums[i] > 2L * nums[j]) {
+                j++;
+            }
+            count += (j - (mid + 1));
+        }
+        return count;
+    }
+
+    private  void merge(int[] nums, int left, int mid, int right) {
+        List<Integer> temp = new ArrayList<>();
+        int i = left, j = mid + 1;
+        while (i <= mid && j <= right) {
+            if (nums[i] <= nums[j]) {
+                temp.add(nums[i++]);
+            } else {
+                temp.add(nums[j++]);
+            }
+        }
+        while (i <= mid) temp.add(nums[i++]);
+        while (j <= right) temp.add(nums[j++]);
+
+        for (int k = left; k <= right; k++) {
+            nums[k] = temp.get(k - left);
+        }
+    }
+}
