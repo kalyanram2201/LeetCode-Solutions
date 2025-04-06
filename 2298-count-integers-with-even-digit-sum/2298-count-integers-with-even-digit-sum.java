b8d34e7f15d99523eefa884a8fa614cfa773acab
Akula Kalyanram <kalyanramakula13@gmail.com>
2025-04-06 18:44:27 +0530

    Solved: 2298. Count Integers With Even Digit Sum

---
+++ 2298-count-integers-with-even-digit-sum/2298-count-integers-with-even-digit-sum.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int countEven(int num) {
+        int count = 0;
+        for (int i = 1; i <= num; i++) {
+            if (digitSum(i) % 2 == 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private int digitSum(int x) {
+        int sum = 0;
+        while (x > 0) {
+            sum += x % 10;
+            x /= 10;
+        }
+        return sum;
+    }
+}
