02b2be8b24c6d89db62e2a4ae7f3f454da69ea78
Akula Kalyanram <kalyanramakula13@gmail.com>
2025-04-13 21:44:33 +0530

    Solved: 2050. Count Good Numbers

---
+++ 2050-count-good-numbers/2050-count-good-numbers.java
@@ -0,0 +1,23 @@
+class Solution {
+
+    long mod = 1000000007;
+
+    public int countGoodNumbers(long n) {
+        return (int) ((helper(5, (n + 1) / 2) * helper(4, n / 2)) % mod);
+    }
+
+
+    public long helper(int x, long y) {
+        long ret = 1;
+        long mul = x;
+        while (y > 0) {
+            if (y % 2 == 1) {
+                ret = (ret * mul) % mod;
+            }
+            mul = (mul * mul) % mod;
+            y /= 2;
+        }
+
+        return ret;
+    }
+}
