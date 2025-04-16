0f624279f50dc46cdc493c4a5b8bb495753e8338
Akula Kalyanram <kalyanramakula13@gmail.com>
2025-04-16 19:37:15 +0530

    Solved: 0290. Word Pattern

---
+++ 0290-word-pattern/0290-word-pattern.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        Map<Character, String> map = new HashMap<>();
+        String[] words = s.split(" ");
+        
+        if (pattern.length() != words.length) return false;
+
+        for (int i = 0; i < pattern.length(); i++) {
+            char ch = pattern.charAt(i);
+            String word = words[i];
+
+            if (map.containsKey(ch)) {
+                if (!map.get(ch).equals(word)) return false;
+            } else {
+                if (map.containsValue(word)) return false;
+                map.put(ch, word);
+            }
+        }
+        return true;
+    }
+}
