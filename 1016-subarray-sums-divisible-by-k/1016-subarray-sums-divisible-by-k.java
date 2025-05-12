4f7463c62b40e9915787f0a12f19c881fb53a6c6
Akula Kalyanram <kalyanramakula13@gmail.com>
2025-05-12 12:55:47 +0530

    Solved: 1016. Subarray Sums Divisible by K

---
+++ 1016-subarray-sums-divisible-by-k/1016-subarray-sums-divisible-by-k.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int subarraysDivByK(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        int sum = 0, count = 0;
+        
+        for (int num : nums) {
+            sum += num;
+            int mod = (sum % k + k) % k;
+            count += map.getOrDefault(mod, 0);
+            map.put(mod, map.getOrDefault(mod, 0) + 1);
+        }
+        return count;
+    }
+}
