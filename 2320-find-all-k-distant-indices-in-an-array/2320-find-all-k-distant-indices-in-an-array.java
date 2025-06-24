022ed49463830f2fc2e4b243269b5f0b0556062f
Akula Kalyanram <kalyanramakula13@gmail.com>
2025-06-24 17:03:50 +0530

    Solved: 2320. Find All K-Distant Indices in an Array

---
+++ 2320-find-all-k-distant-indices-in-an-array/2320-find-all-k-distant-indices-in-an-array.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<Integer> findKDistantIndices(int[] nums, int key, int k) {
+        int n = nums.length;
+        boolean valid[] = new boolean[n];
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == key) {
+
+                int start = Math.max(0, i - k);
+                int end = Math.min(n - 1, i + k);
+
+                for (int j = start; j <= end; j++) {
+                    valid[j] = true;
+                }
+            }
+        }
+
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (valid[i]) {
+                result.add(i);
+            }
+        }
+        return result;
+    }
+}
