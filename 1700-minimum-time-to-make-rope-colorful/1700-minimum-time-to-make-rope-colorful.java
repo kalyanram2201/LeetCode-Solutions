b870777d7a66197c21bb06117cd9a8e29241eba2
Akula Kalyanram <kalyanramakula13@gmail.com>
2025-11-03 19:02:30 +0530

    Solved: 1700. Minimum Time to Make Rope Colorful

---
+++ 1700-minimum-time-to-make-rope-colorful/1700-minimum-time-to-make-rope-colorful.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minCost(String colors, int[] neededTime) {
+        int totalCost = 0;
+        int maxTime = neededTime[0];
+
+        for (int i = 1; i < colors.length(); i++) {
+            if (colors.charAt(i) == colors.charAt(i - 1)) {
+                totalCost += Math.min(maxTime, neededTime[i]);
+                maxTime = Math.max(maxTime, neededTime[i]);
+            } else {
+                maxTime = neededTime[i];
+            }
+        }
+
+        return totalCost;
+    }
+}
