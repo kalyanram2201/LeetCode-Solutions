3698b777afcd724b2bc31ce29d174bdad64912e7
Akula Kalyanram <kalyanramakula13@gmail.com>
2025-12-28 11:38:50 +0530

    Solved: 2233. Number of Smooth Descent Periods of a Stock

---
+++ 2233-number-of-smooth-descent-periods-of-a-stock/2233-number-of-smooth-descent-periods-of-a-stock.java
@@ -0,0 +1,17 @@
+class Solution {
+    public long getDescentPeriods(int[] prices) {
+        long count = 0;
+        long temp = 0;
+        count += prices.length;
+        
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i - 1] - prices[i] == 1) {
+                temp++;
+                count += temp;
+            } else {
+                temp = 0;
+            }
+        }
+        return count;
+    }
+}
