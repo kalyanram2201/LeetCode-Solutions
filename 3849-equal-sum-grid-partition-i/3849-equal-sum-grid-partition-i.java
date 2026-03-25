664b9b79f195f78a8d8c33cb5210414d3155a73f
Akula Kalyanram <kalyanramakula13@gmail.com>
2026-03-25 22:35:59 +0530

    Solved: 3849. Equal Sum Grid Partition I

---
+++ 3849-equal-sum-grid-partition-i/3849-equal-sum-grid-partition-i.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean canPartitionGrid(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        long total = 0;
+
+        for (int[] row : grid) {
+            for (int val : row) {
+                total += val;
+            }
+        }
+        if ((total & 1) == 1)
+            return false;
+
+        long goal = total / 2, sum = 0;
+        for (int i = 0; i < m - 1; i++) {
+            for (int j = 0; j < n; j++) {
+                sum += grid[i][j];
+            }
+            if (sum == goal)
+                return true;
+        }
+        sum = 0;
+        for (int j = 0; j < n - 1; j++) {
+            for (int i = 0; i < m; i++) {
+                sum += grid[i][j];
+
+            }
+            if (sum == goal)
+                return true;
+        }
+        return false;
+    }
+}
