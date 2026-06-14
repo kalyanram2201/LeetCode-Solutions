0a4ff9e9ccd1f7c974cce283a19e02d589bdce87
Akula Kalyanram <kalyanramakula13@gmail.com>
2026-06-14 23:50:53 +0530

    Solved: 2236. Maximum Twin Sum of a Linked List

---
+++ 2236-maximum-twin-sum-of-a-linked-list/2236-maximum-twin-sum-of-a-linked-list.java
@@ -0,0 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int pairSum(ListNode head) {
+        if (head == null || head.next == null)
+            return 0;
+
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode revHead = reverse(slow);
+
+        int max = 0;
+        ListNode node = head;
+        while (revHead != null) {
+            int sum = node.val + revHead.val;
+            max = Math.max(max, sum);
+            node = node.next;
+            revHead = revHead.next;
+        }
+
+        return max;
+    }
+
+    private ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    }
+}
