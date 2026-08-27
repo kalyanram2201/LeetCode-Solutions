9f00979a2a34e2eb563e9a5b2d3004ae5672aa3e
Kalyanram <kalyanramakula13@gmail.com>
2026-08-27 19:07:39 +0530

    Solved: 1833. Find the Highest Altitude (Easy)

---
+++ 1833-find-the-highest-altitude/1833-find-the-highest-altitude.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int largestAltitude(int[] gain) {
+        int max=0;
+        int cur=0;
+        for(int i=0;i<gain.length;i++)
+        {
+            cur+=gain[i];
+            max=Math.max(max,cur);
+        }
+        return max;
+        
+    }
+}
