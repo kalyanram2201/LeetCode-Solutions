ea0e7e3c47a1131539496215b437e93634aee0ca
Kalyanram <kalyanramakula13@gmail.com>
2026-08-28 19:50:23 +0530

    Solved: 164. Maximum Gap (Medium)

---
+++ 0164-maximum-gap/0164-maximum-gap.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maximumGap(int[] nums) {
+        if (nums.length < 2)
+            return 0;
+
+        Arrays.sort(nums);
+        int max = 0;
+        for (int i = nums.length - 1; i >= 1; i--) {
+            max = Math.max(nums[i] - nums[i - 1], max);
+        }
+        return max;
+    }
+}
