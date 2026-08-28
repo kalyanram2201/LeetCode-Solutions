8170da0a6a13828eaf47fb9a75c920532bc06cbf
Kalyanram <kalyanramakula13@gmail.com>
2026-08-28 19:12:14 +0530

    Solved: 1306. Minimum Absolute Difference (Easy)

---
+++ 1306-minimum-absolute-difference/1306-minimum-absolute-difference.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<List<Integer>> minimumAbsDifference(int[] arr) {
+        Arrays.sort(arr);
+
+        int min = Integer.MAX_VALUE;
+        List<List<Integer>> result = new ArrayList<>();
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            int diff = arr[i + 1] - arr[i];
+
+            if (diff < min) {
+                min = diff;
+                result.clear();
+                result.add(Arrays.asList(arr[i], arr[i + 1]));
+            } else if (diff == min) {
+                result.add(Arrays.asList(arr[i], arr[i + 1]));
+            }
+        }
+
+        return result;
+    }
+}
