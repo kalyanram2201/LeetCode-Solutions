6e22869e3beb3c7b71c491d1571c1fd663170ca5
Kalyanram <kalyanramakula13@gmail.com>
2026-08-28 20:14:13 +0530

    Solved: 3621. Minimum Operations to Make Array Values Equal to K (Easy)

---
+++ 3621-minimum-operations-to-make-array-values-equal-to-k/3621-minimum-operations-to-make-array-values-equal-to-k.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int minOperations(int[] nums, int k) {
+        int mini = Integer.MAX_VALUE;
+        for (int num : nums) mini = Math.min(mini, num);
+        if (mini < k) return -1;
+
+        HashSet<Integer> s = new HashSet<>();
+        for (int num : nums) s.add(num);
+        return s.size() - (mini == k ? 1 : 0);
+    }
+}
