4c3c6d21b6f37290cf55eaada0fb82c3785c158a
Kalyanram <kalyanramakula13@gmail.com>
2026-08-28 19:49:52 +0530

    Solved: 133. Clone Graph (Medium)

---
+++ 0133-clone-graph/0133-clone-graph.java
@@ -0,0 +1,21 @@
+class Solution {
+    Map<Node, Node> map = new HashMap<>();
+
+    public Node cloneGraph(Node node) {
+        if (node == null)
+            return null;
+
+        if (map.containsKey(node)) {
+            return map.get(node);
+        }
+
+        Node clone = new Node(node.val, new ArrayList<>());
+        map.put(node, clone);
+
+        for (Node neighbor : node.neighbors) {
+            clone.neighbors.add(cloneGraph(neighbor));
+        }
+
+        return clone;
+    }
+}
