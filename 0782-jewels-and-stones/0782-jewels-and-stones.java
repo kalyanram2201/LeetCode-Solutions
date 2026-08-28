1879174cfde4ceca760e481c00de7bf1aa97dc59
Kalyanram <kalyanramakula13@gmail.com>
2026-08-28 20:09:46 +0530

    Solved: 782. Jewels and Stones (Easy)

---
+++ 0782-jewels-and-stones/0782-jewels-and-stones.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        Set<Character> set = new HashSet<>();
+        for (char c : jewels.toCharArray()) {
+            set.add(c);
+        }
+
+        int count = 0;
+        for (char ch : stones.toCharArray()) {
+            if (set.contains(ch))
+                count++;
+        }
+        return count;
+    }
+}
