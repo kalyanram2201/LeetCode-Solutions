000d5c1bb72f9d082e57e5b82ecbd75153ec29a9
Kalyanram <kalyanramakula13@gmail.com>
2026-08-28 19:22:41 +0530

    Solved: 191. Number of 1 Bits (Easy)

---
+++ 0191-number-of-1-bits/0191-number-of-1-bits.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int hammingWeight(int n) {
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return 1;
+        int count = 0;
+        while (n > 0) {
+            if ((n & 1) == 1) {
+                count++;
+            }
+            n = n >> 1;
+        }
+        return count;
+    }
+}
