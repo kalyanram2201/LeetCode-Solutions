3e1349b20471fd1ca4d43049b2f49d7c7a22deaa
Kalyanram <kalyanramakula13@gmail.com>
2026-08-28 20:11:20 +0530

    Solved: 228. Summary Ranges (Easy)

---
+++ 0228-summary-ranges/0228-summary-ranges.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<String> summaryRanges(int[] nums) {
+        List<String> list=new ArrayList<>();
+        if(nums.length==0)return list;
+        
+        int start=nums[0];
+        for(int i=1;i<=nums.length;i++){
+            if(i==nums.length || nums[i-1]+1!=nums[i]){
+                if(start==nums[i-1]){
+                    list.add(String.valueOf(start));
+                }
+                else{
+                    list.add(start+"->"+nums[i-1]);
+                }
+                if(i<nums.length)start=nums[i];
+            }
+        }
+        return list;
+    }
+}
