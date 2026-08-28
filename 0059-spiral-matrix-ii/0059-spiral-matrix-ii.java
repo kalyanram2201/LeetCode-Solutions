ebfc5d028f010b1296eb9de336d7606f03c82628
Kalyanram <kalyanramakula13@gmail.com>
2026-08-28 19:58:12 +0530

    Solved: 59. Spiral Matrix II (Medium)

---
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int[][] generateMatrix(int n) {
+        int[][] result = new int[n][n];
+        int top = 0, bottom = n - 1;
+        int left = 0, right = n - 1, k = 1;
+
+        while (top <= bottom && left <= right) {
+
+            for (int i = left; i <= right; i++) {
+                result[top][i] = k++;
+            }
+            top++;
+
+            for (int i = top; i <= bottom; i++) {
+                result[i][right] = k++;
+            }
+            right--;
+
+            if (top <= bottom) {
+
+                for (int i = right; i >= left; i--) {
+                    result[bottom][i] = k++;
+                }
+                bottom--;
+            }
+
+            if (left <= right) {
+
+                for (int i = bottom; i >= top; i--) {
+                    result[i][left] = k++;
+                }
+                left++;
+            }
+        }
+        return result;
+    }
+}
