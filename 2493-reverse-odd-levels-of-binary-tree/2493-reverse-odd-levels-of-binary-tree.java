03af09e8c0d90f6117830ef72e928fef71729177
Kalyanram <kalyanramakula13@gmail.com>
2026-08-28 20:04:24 +0530

    Solved: 2493. Reverse Odd Levels of Binary Tree (Medium)

---
+++ 2493-reverse-odd-levels-of-binary-tree/2493-reverse-odd-levels-of-binary-tree.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode reverseOddLevels(TreeNode root) {
+        if (root == null) return null;
+       helper(root.left, root.right, 1);
+        return root;
+    }
+
+    private void helper(TreeNode left, TreeNode right, int level) {
+        if (left == null || right == null) return;
+
+        if (level % 2 == 1) {
+            int temp = left.val;
+            left.val = right.val;
+            right.val = temp;
+        }
+
+       helper(left.left, right.right, level + 1);
+       helper(left.right, right.left, level + 1);
+    }
+}
