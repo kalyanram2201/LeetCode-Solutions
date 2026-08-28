dcd9698d44863d2a3e1964e3ee578516bb4b3e73
Kalyanram <kalyanramakula13@gmail.com>
2026-08-28 19:37:09 +0530

    Solved: 3462. Vowels Game in a String (Medium)

---
+++ 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean doesAliceWin(String s) {
+
+        for (char ch : s.toCharArray()) {
+            switch (ch) {
+                case 'a', 'e', 'i', 'o', 'u':
+                    return true;
+            }
+        }
+
+        return false;
+    }
+}
